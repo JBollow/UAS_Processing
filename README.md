f940e24e83017330cd5972b682d9797987964324
JBollow <Jan_PatrickBollow@hotmail.com>
2018-06-20 14:48:02 +0200

    License and portstuff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # UAS_Processing
 Processing and Analysis Workflows for UAS-Borne Spatial Data
 
+## License
+
+MIT License
+
+Copyright (c) 2018 UAS_Processing
--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,6 @@ app.listen(port, function (err, res) {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // CORS to avoid cross-origin errors
@@ -29,16 +28,20 @@ app.use(function (req, res, next) {
 });
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
