d1974062950770b2587ac0a4dbcd62792193ffa8
JBollow <Jan_PatrickBollow@hotmail.com>
2018-06-20 15:54:22 +0200

    shell exec qgis random

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 
@@ -18,6 +19,7 @@ app.listen(port, function (err, res) {
 });
 
 // view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // CORS to avoid cross-origin errors
@@ -28,14 +30,16 @@ app.use(function (req, res, next) {
 });
 
 app.use(logger('dev'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
